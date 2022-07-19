6f0a463423c26bab3dcd1a61cb8976c3de7baba6
Oluwasanmi Idowu <idowuoluwasanmi@gmail.com>
2022-07-19 11:04:07 +0100

    final update

--- I4G020055CK3/links/views.py
+++ I4G020055CK3/links/views.py
@@ -6,11 +6,7 @@ from links.serializers import LinkSerializer
 from .models import Link
 from . import models
 from . import serializers
-from rest_framework.generics import ListAPIView
-from rest_framework.generics import CreateAPIView
-from rest_framework.generics import RetrieveAPIView
-from rest_framework.generics import UpdateAPIView
-from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
